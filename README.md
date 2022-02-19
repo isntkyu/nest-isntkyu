f9762b90206a398e207ab1f1fcc5605303f816b4
isntkyu <jkjkjk5710@gmail.com>
2022-02-19 12:12:26 +0900

    mo, s, co 생성

--- README.md
+++ README.md
@@ -16,16 +16,7 @@ express로 구성된 백엔드 코드를 nestjs로 바꿔봅니다
     * implements 를 하면 구현이 반드시 이루어져야함
     * DI provider - @injectable()
 
-
-  >  users 모듈만들고 > 컨트롤러 
-
-  - nest g(generate) mo users
-  - nest g co users 컨트로러
-  - nest g s users 서비싀
-
-  #### nest 명령어 대신 npx @nestjs/cli 를 사용하는중
-
-  npm i -g @nestjs/cli는 문제가있음 (나중에 정리, 해결)
-
-  대부분의 경우 npx항상 최신 버전을 사용하고 자주 필요하지 않은 항목으로 파일 시스템을 복잡하게 만들지 않기 때문에 전역 npm 패키지를 설치하는 대신 사용하는 것이 좋습니다.
-  https://stackoverflow.com/questions/57266622/unable-to-create-a-new-project-with-the-nest-cli
+* 2/19
+  
+  * 모듈 , 컨트롤러 생성
+  * https://velog.io/@isntkyu/NestJS 정리
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule, ChannelsModule, DmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
