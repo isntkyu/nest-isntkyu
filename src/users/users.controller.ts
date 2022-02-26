c201bc32332cfda252729adbe0d93af85028ab68
isntkyu <jkjkjk5710@gmail.com>
2022-02-26 17:23:44 +0900

    @nesjs/passport

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post, Req, Res, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { User } from 'src/common/decorators/user.decorator';
 import { UserDto } from 'src/common/dto/user.dto';
 import { UndefinedToNullInterceptor } from 'src/common/interceptors/undefinedToNull.interceptor';
@@ -35,6 +36,7 @@ export class UsersController {
         type: UserDto
     })
     @ApiOperation({summary: '로그인'})
+    @UseGuards(LocalAuthGuard)
     @Post('login')
     logIn(@User() user) {
         return user;
